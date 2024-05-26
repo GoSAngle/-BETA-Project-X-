8f0c97f97eee41d5c88790776df371692107fd4b
ForgeInTheNet <165851538+GoSAngle@users.noreply.github.com>
2024-05-26 18:17:02 +0000

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,42 @@
+Instaler() { 
+   clear
+echo "[INSTALADOR] Bienvenido al instalador de 'Project X', Que sistema tiene?."
+echo ">-----------------------------------------------------------------------<"
+echo " Usted tiene LINUX? (Y/n)"
+read -p "> " LinuxChoice
+AND() {
+   cd ./.Plataforms
+     bash ANDROID.sh
+
+}
+}
+Instaler
+
+if [[ "$LinuxChoice" = "Y" || "$LinuxChoice" = "y" ]]; then
+        echo ;
+        for i in {1..5}; do
+
+             echo "[SERVICO] Instalando."
+             sleep 0.5
+             clear
+             echo "[SERVICIO] Instalando.."
+             sleep 0.5
+             clear
+             echo "[SERVICIO] Instalando..."
+             sleep 0.5
+             clear
+         done
+
+                # Instalador del paquete
+         sleep 0.5
+         echo "Instalado!."
+
+      elif [[ "$LinuxChoice" = "N" || "$LinuxChoice" = "n" ]]; then
+          echo ;
+           AND
+      else
+        echo "Incorrecto, responda Y / n"
+        sleep 1
+        Instaler
+fi
+
